5d307e784d548674016ec781cc0da85edbf6eef1
Kevin Torres Martínez <torresmtzkevin@gmail.com>
2021-09-11 16:26:45 -0500

    Routing & Appbar (#15)

---
+++ src/containers/Homepage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Homepage = () => (
+  <div>
+    <header>
+      <h1>Homepage</h1>
+    </header>
+  </div>
+);
+
+export default Homepage;
---
+++ src/containers/Landingpage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Landingpage = () => (
+  <div>
+    <header>
+      <h1>Landingpage</h1>
+    </header>
+  </div>
+);
+
+export default Landingpage;
