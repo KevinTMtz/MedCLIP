4b6289547821cf100664697c8a9301ac8db1a445
KevinTMtz <torresmtzkevin@gmail.com>
2021-09-16 14:32:52 -0500

    Tab navigation

--- src/containers/Homepage.tsx
+++ src/containers/Homepage.tsx
@@ -1,11 +1,43 @@
 import React from 'react';
+import { Box, Tab, Tabs } from '@material-ui/core';
 
 import Cases from './cases/Cases';
+import Diagnostics from './diagnostics/Diagnostics';
+import TabPanel from '../components/navigation/TabPanel';
 
 const Homepage = () => {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (_: any, newValue: any) => {
+    setValue(newValue);
+  };
+
   return (
     <div>
-      <Cases />
+      <Box sx={{ width: '100%' }}>
+        <Box
+          sx={{
+            borderColor: 'primary',
+            borderBottom: '1px solid #e8e8e8',
+          }}
+        >
+          <Tabs
+            variant='fullWidth'
+            indicatorColor='primary'
+            value={value}
+            onChange={handleChange}
+          >
+            <Tab label='Diagnostics' />
+            <Tab label='My Cases' />
+          </Tabs>
+        </Box>
+        <TabPanel value={value} index={0}>
+          <Diagnostics />
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          <Cases />
+        </TabPanel>
+      </Box>
     </div>
   );
 };
