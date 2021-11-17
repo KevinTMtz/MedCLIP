8859878a54a9c40f27dcd1823db67b4be0a9f53a
SebasRod23 <sebastian_rodriguezg@yahoo.com>
2021-11-16 23:06:57 -0600

    Update envirioment variables

--- server/db/connection.ts
+++ server/db/connection.ts
@@ -1,13 +1,22 @@
 import { Sequelize } from 'sequelize';
+import mysql from 'mysql2';
+
 import SERVER from '../config/config';
 
-const host = SERVER.host;
+const db_host = SERVER.db.host;
 const db_name = SERVER.db.name;
 const db_username = SERVER.db.username;
 const db_password = SERVER.db.password;
 
-const db = new Sequelize(db_name, 'root', 'MYSQL_ROOT_PASSWORD', {
-  host: 'mysql_db',
+const connection = mysql.createConnection({
+  host: db_host,
+  user: db_username,
+  password: db_password,
+});
+connection.query(`CREATE DATABASE IF NOT EXISTS \`${db_name}\`;`);
+
+const db = new Sequelize(db_name, db_username, db_password, {
+  host: db_host,
   dialect: 'mysql',
   logging: false,
 });
